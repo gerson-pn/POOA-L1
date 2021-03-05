b0869b5c2c0e8ebb8f2197a3c229f0ed1c86020a
gpn <gpn@host.docker.internal>
2021-03-05 10:44:34 -0300

    Inclusão do banco de dados

---
+++ l1/src/main/java/org/fatec/l1/controller/CadastroController.java
@@ -0,0 +1,25 @@
+package org.fatec.l1.controller;
+
+import org.fatec.l1.domain.Cliente;
+import org.fatec.l1.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CadastroController {
+	@Autowired
+	private ClienteRepository cr;
+	
+	@GetMapping("/cadastrar-clientes")
+	public String cadastrarClientes() {
+		return "cadastro";
+	}
+	
+	@PostMapping("/cadastrar-clientes")
+	public String cadastrarClientes(Cliente c) {
+		cr.save(c);
+		return "cadastro";
+	}
+}
---
+++ l1/src/main/java/org/fatec/l1/controller/ListaController.java
@@ -0,0 +1,24 @@
+package org.fatec.l1.controller;
+
+import java.util.List;
+
+import org.fatec.l1.domain.Cliente;
+import org.fatec.l1.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ListaController {
+	@Autowired
+	private ClienteRepository cr;
+	
+	@GetMapping("/listar-clientes")
+	public ModelAndView cadastrarClientes() {
+		List<Cliente> clientes = cr.findAll();
+		ModelAndView mv = new ModelAndView("lista");
+		mv.addObject("clientes", clientes);
+		return mv;
+	}
+}
