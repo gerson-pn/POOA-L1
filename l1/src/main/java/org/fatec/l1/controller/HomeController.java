32bcc48c591684f2f620c53efef43c367159edea
gpn <gpn@DESKTOP-73J6ED3.mshome.net>
2021-03-25 12:18:13 -0300

    Exemplo de autenticação - sem spring security

--- l1/src/main/java/org/fatec/l1/controller/HomeController.java
+++ l1/src/main/java/org/fatec/l1/controller/HomeController.java
@@ -1,13 +1,43 @@
 package org.fatec.l1.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.fatec.l1.domain.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
-	
+
 	@GetMapping("/")
-	public String paginaInicial() {
-		return "index";
+	public String paginaInicial(HttpSession sessao) {
+		// entenda a sessao, como um mapa, ou dicionario
+		Usuario u = (Usuario) sessao.getAttribute("usuario");
+		if(u == null) {
+			return "redirect:login";
+		}else {
+			return "index";	
+		}
+		
 	}
+	
+	@GetMapping("/login")
+	public String autenticacao() {
+		return "login";
+	}
+	
+	@PostMapping("/login")
+	public String autenticar(HttpSession sessao, Usuario u) {
+		// ir no banco, verificar...
+		sessao.setAttribute("usuario", u);
+		return "redirect:/";
+	}
+	
+	@GetMapping("/logout")
+	public String sair(HttpSession sessao) {
+		sessao.invalidate();
+		return "login";
+	}
+	
 }
